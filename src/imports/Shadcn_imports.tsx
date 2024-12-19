d21b654fbda9d6fca7d417d3ed5645c2faa505ed
Mohit Saini <mailtomohit2002@gmail.com>
2024-12-19 21:25:11 +0530

    feat: Added Add employee dialog

---
+++ src/imports/Shadcn_imports.tsx
@@ -0,0 +1,57 @@
+export { Input } from '@/components/ui/input';
+export { Label } from '@/components/ui/label';
+export { Button } from '@/components/ui/button';
+export {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+    CardDescription,
+} from '@/components/ui/card';
+
+export {
+    Select,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+    SelectContent
+} from '@/components/ui/select'
+
+export {
+    Table,
+    TableHeader,
+    TableRow,
+    TableHead,
+    TableBody,
+    TableCell,
+} from '@/components/ui/table';
+
+export {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuItem,
+} from '@/components/ui/dropdown-menu';
+
+
+
+export {
+    Dialog,
+    DialogContent,
+    DialogTrigger,
+    DialogClose,
+} from '@/components/ui/dialog';
+
+
+export {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from '@/components/ui/tooltip';
+
+export { Skeleton } from '@/components/ui/skeleton';
+
